9406992c338270ddbdde54251a3f64359eae8da8
Scott Rodriguez <scottmrodriguez51@gmail.com>
2020-07-09 20:21:51 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,21 @@ const codes = [
 ];
 
 function init() {
- 
+ let index = 0;
+
+  document.body.addEventListener("keydown", function(e) {
+    const key = e.key;
+
+    if (codes[index] === key) {
+      index++;
+
+      if (index === codes.length) {
+        alert("Hurray!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
+}
 }
