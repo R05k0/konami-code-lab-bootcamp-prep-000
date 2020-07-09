badd2032c1abca87507b82a90d7768f29749d905
Scott Rodriguez <scottmrodriguez51@gmail.com>
2020-07-09 20:22:40 +0000

    Automatically backed up by Learn

---
+++ konami_code.js
@@ -0,0 +1,31 @@
+const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
+
+function init() {
+  let index = 0;
+
+  document.body.addEventListener("keydown", function(e) {
+    const key = e.key;
+
+    if (codes[index] === key) {
+      index++;
+
+      if (index === codes.length) {
+        alert("Hurray!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
+}
